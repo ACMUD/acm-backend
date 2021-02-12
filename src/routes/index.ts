2137ab168ca85a929af82ee47a0978356689ebcb
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-12 09:19:48 -0500

    Add middlewates to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,22 @@
 import { Router, Request, Response } from 'express';
+import error404 from '../middlewares/error404';
+import errorHandler from '../middlewares/errorHandler';
 
+// Main Router
 const apiRouter = Router();
 
+// Add API Routes
 apiRouter.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hola mundo' });
+  res.send({ message: 'Hola mundo' });
 });
 
 apiRouter.post('/', (req: Request, res: Response) => {
-  res.json(req.body);
+  const data = req.body;
+  res.send(data);
 });
+
+// Add MiddleWares
+apiRouter.use(error404);
+apiRouter.use(errorHandler);
 
 export default apiRouter;
