13fc58113d75c417bf401fbfdb98d3487c12b057
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-12 22:24:59 -0500

    add authDTO for token

--- src/modules/users/controllers/tokenController.ts
+++ src/modules/users/controllers/tokenController.ts
@@ -1,24 +1,17 @@
 import { sign, verify } from 'jsonwebtoken';
 import { Account } from '../entities/Account';
+import { authDTO } from '../dtos/authDTO';
 
-async function generateJWT(value: string | Account) {
-  let userId;
-  if (typeof value === 'string') {
-    userId = value;
-  } else {
-    userId = value.id;
-  }
-
+async function generateJWT(account: Account) {
   const SECRET = process.env.JWT_SECRET || 'supersecrettoken';
-  return sign({ userId }, SECRET);
+  return sign({ accountId: account.id }, SECRET);
 }
 
 async function verifyJWT(jwt: string) {
   const SECRET = process.env.JWT_SECRET || 'supersecrettoken';
-  const userId = verify(jwt, SECRET);
-  if (!userId) throw new Error('Invalid Token');
-
-  return userId;
+  const auth = verify(jwt, SECRET);
+  if (!auth) throw new Error('Invalid Token');
+  return auth as authDTO;
 }
 
 export { generateJWT, verifyJWT };
