b22bdd9f24681fa164aa375d489fe9f3c7cd13fb
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-14 21:13:15 -0500

    export graphql resovlers

--- src/modules/users/graphql/resolvers/index.ts
+++ src/modules/users/graphql/resolvers/index.ts
@@ -1,3 +1,6 @@
-const usersResolvers: Function[] = [];
+import { authResolver } from './auth';
+import { profileResolver } from './profile';
+
+const usersResolvers: Function[] = [authResolver, profileResolver];
 
 export { usersResolvers };
