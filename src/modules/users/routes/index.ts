f0a1a7156ba6f9cc5f655d54e19970eb1484e08a
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-16 20:44:36 -0500

    Add google Routes

--- src/modules/users/routes/index.ts
+++ src/modules/users/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { authRouter } from './auth';
+import { googleAuthRouter } from './googleAuth';
 import { profileRouter } from './profile';
 
 const usersRouter = Router();
 
 usersRouter.use('/auth', authRouter);
+usersRouter.use('/auth/google', googleAuthRouter);
 usersRouter.use('/profile', profileRouter);
 
 export { usersRouter };
