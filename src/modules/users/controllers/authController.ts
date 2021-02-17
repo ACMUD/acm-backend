13437762f895a87b4b68ac272bde1069c3e99391
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-16 20:44:18 -0500

    Add google controllers in AuthController

--- src/modules/users/controllers/authController.ts
+++ src/modules/users/controllers/authController.ts
@@ -1,6 +1,17 @@
+import { OAuth2Client } from 'google-auth-library';
 import { authDTO } from '../dtos/authDTO';
-import { createAccount, verifyAccount } from './accountController';
+import {
+  createAccount,
+  verifyAccount,
+  getAccountByEmail,
+} from './accountController';
 import { getTokens } from './tokenController';
+import { updateMe } from './profileController';
+
+const googleID =
+  process.env.CLIENT_ID ||
+  '953529045812-c4v3qohqmaug0vtbek0n99fcibdvt0kf.apps.googleusercontent.com';
+const googleClient = new OAuth2Client(googleID);
 
 async function signup({ email, password }: authDTO) {
   if (!email || !password) throw new Error('Invalid Credentials');
@@ -16,4 +27,34 @@ async function login({ email, password }: authDTO) {
   return await getTokens(loggedUSer);
 }
 
-export { signup, login };
+async function signWithGoogle(idToken: string) {
+  const ticket = await googleClient.verifyIdToken({
+    audience: googleID,
+    idToken,
+  });
+  const { given_name, family_name, email, picture } = ticket.getPayload()!;
+  const randomPassword = ''; //randomPassword();
+  const createdAccount = await createAccount(email!, randomPassword);
+
+  const { id } = createdAccount.userProfile;
+  await updateMe(`${id}`, {
+    firstName: given_name,
+    lastName: family_name,
+    imageUrl: picture,
+  });
+
+  return await getTokens(createdAccount);
+}
+
+async function loginWithGoogle(idToken: string) {
+  const ticket = await googleClient.verifyIdToken({
+    audience: googleID,
+    idToken,
+  });
+  const { email } = ticket.getPayload()!;
+  const existingAccount = await getAccountByEmail(email!);
+
+  return await getTokens(existingAccount);
+}
+
+export { signup, login, signWithGoogle, loginWithGoogle };
