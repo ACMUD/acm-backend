b7179131e4b40fc14791f9e46463965abb0df107
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-17 23:12:45 -0500

    Fix Routes

--- src/modules/users/routes/index.ts
+++ src/modules/users/routes/index.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import { authRouter } from './auth';
-import { googleAuthRouter } from './googleAuth';
 import { profileRouter } from './profile';
 
 const usersRouter = Router();
 
-usersRouter.use('/auth', authRouter);
-usersRouter.use('/auth/google', googleAuthRouter);
 usersRouter.use('/profile', profileRouter);
 
 export { usersRouter };
