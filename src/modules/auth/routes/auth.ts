00ac05cae2880ecf5d75884f800afb62dea1aa39
Edison Peñuela <edisonpe961206@hotmail.com>
2021-02-19 09:55:12 -0500

    remove duplicated refresh_token endpoint in basicAuthRouter

--- src/modules/auth/routes/auth.ts
+++ src/modules/auth/routes/auth.ts
@@ -1,20 +1,11 @@
 import { Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
-
 import { login, signup, verifySingup } from '../controllers/authController';
-import { getAccountById } from '../controllers/accountController';
-
 import { addRefreshToken } from '../utils/refreshCookie';
-import { verifyRefreshToken } from 'services/jwt';
-import { getTokens } from '../utils/generateTokens';
 
-import {
-  handleBadRequestError,
-  handleUnauthorizedError,
-} from 'utils/handleError';
+import { handleBadRequestError } from 'utils/handleError';
 
 const basicAuthRouter = Router();
-const REFRESH_TOKEN_ID = process.env.REFRESH_TOKEN_ID || 'jid';
 
 basicAuthRouter.post('/signup', async (req: Request, res: Response) => {
   try {
@@ -68,30 +59,6 @@ basicAuthRouter.post('/logout', async (req: Request, res: Response) => {
   res.send({
     message: 'Successfull Logout',
   });
-});
-
-// Refresh Token EndPoint
-basicAuthRouter.post('/refresh_token', async (req: Request, res: Response) => {
-  const token = req.cookies[REFRESH_TOKEN_ID];
-  if (!token) {
-    return handleUnauthorizedError(res);
-  }
-
-  try {
-    const { accountId } = await verifyRefreshToken(token);
-    const account = await getAccountById(accountId);
-    const [accessToken, refreshToken] = await getTokens(account);
-    addRefreshToken(res, refreshToken);
-
-    const response = {
-      message: 'Successfull Generated Token',
-      accessToken,
-    };
-
-    res.send(response);
-  } catch (error) {
-    handleBadRequestError(res, error);
-  }
 });
 
 export { basicAuthRouter };
