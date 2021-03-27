677decae8eaac6af1d31a48d1c7db0ea211ac711
Edison Peñuela <edisonpe961206@hotmail.com>
2021-03-27 09:36:08 -0500

    Add multer error handler exception

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,3 +1,4 @@
+import multer from 'multer';
 import { NextFunction, Request, Response } from 'express';
 import { getReasonPhrase, StatusCodes } from 'http-status-codes';
 
@@ -8,6 +9,12 @@ function errorHandler(
   next: NextFunction
 ) {
   console.error(err.stack);
+  if (err instanceof multer.MulterError) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send({ message: err.message });
+  }
+
   res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .send({ message: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR) });
